28994f4d69b37e0436c9a9bc364e7a2906fbb033
chuanyi.huang <hcydxz@gmail.com>
2019-03-23 18:23:43 -0500

    valid_brackets

--- largest_num_from_array.py
+++ largest_num_from_array.py
@@ -3,11 +3,24 @@ given a [int], only contains no-negative numbers, catancate them as a whole, ret
 Given two numbers X and Y, how should myCompare() decide which number to put first –
 we compare two numbers XY (Y appended at the end of X) and YX (X appended at the end of Y)
 """
+from functools import cmp_to_key
 
 
-def main():
-    pass
+@cmp_to_key
+def comparator(a, b):
+    ab = str(a) + str(b)
+    ba = str(b) + str(a)
+    # return ((int(ba) > int(ab)) - (int(ba) < int(ab)))  True - False
+    if ab == ba:
+        return 0
+    elif ab > ba:
+        return -1
+    else:
+        return 1
 
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    a = [54, 546, 548, 60]
+    sorted_array = sorted(a, key=comparator)
+    number = "".join([str(i) for i in sorted_array])
+    print(number)
--- valid_brackets.py
+++ valid_brackets.py
@@ -1,3 +1,34 @@
 """
 given a positive int n, return str with n '(' or ')' which are valid.
 """
+
+
+def printParenthesis(str, n):
+    if (n > 0):
+        _printParenthesis(str, 0,
+                          n, 0, 0)
+    return
+
+
+def _printParenthesis(str, pos, n,
+                      open, close):
+    if (close == n):
+        for i in str:
+            print(i, end="")
+        print()
+        return
+    else:
+        if (open > close):
+            str[pos] = '}'
+            _printParenthesis(str, pos + 1, n,
+                              open, close + 1)
+        if (open < n):
+            str[pos] = '{'
+            _printParenthesis(str, pos + 1, n,
+                              open + 1, close)
+
+
+if __name__ == '__main__':
+    n = 3
+    str = [""] * 2 * n
+    printParenthesis(str, n)
