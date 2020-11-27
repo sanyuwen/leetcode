cda5cdeb65933118bd95bf05c0c6a44db4a5def9
Chuanyi Huang <102843@smsassist.com>
2020-11-27 17:12:16 -0600

    adding oa4

--- oa4.py
+++ oa4.py
@@ -5,20 +5,39 @@
 
 def solve(s: str) -> str:
     # dp[i][j], 0<=i<=j<len(s), true if s[i..j] is palindrome
+    # insight from https://leetcode.com/problems/longest-palindromic-substring/
     n = len(s)
-    dp = [[True] * n for _ in range(n)]
-    ans = (0, 1)
+    dp = [[False] * n for _ in range(n)]
     for i in range(n - 1):
-        dp[i][i + 1] = True if s[i] == s[i + 1] else False
-        if dp[i][i + 1]:
-            ans = (i, i + 2)
+        dp[i][i] = True
+        if s[i] == s[i + 1]:
+            dp[i][i + 1] = True
+    dp[n - 1][n - 1] = True
     for t in range(2, n):
         for i in range(n - t):
-            dp[i][i + t] = True if s[i] == s[i + t] and dp[i + 1][i + t - 1] else False
-            if dp[i][i + t]:
-                ans = (i, i + t + 1)
+            if s[i] == s[i + t] and dp[i + 1][i + t - 1]:
+                dp[i][i + t] = True
+    # utilize information above, we solve this problem
+    # for r in dp:
+    #     print(r)
+    idx = 0
+    while idx < n - 1:
+        i = n - 1
+        while i > idx and not dp[idx][i]:
+            i -= 1
+        if i == idx:
+            # no palindrome any more
+            break
+        idx = i + 1
+    return s[idx:]
 
 
 if __name__ == '__main__':
     s = "babddc"
     assert solve(s) == "c"
+
+    s = "babbabdc"
+    assert solve(s) == "dc"
+
+    s = "bbaabbdaccadca"
+    assert solve(s) == "ca"
